0b5790ed2cca1577e04c7b24ece6d491ae58064d
rakesh-barik <connect2rakeshbarik@gmail.com>
2015-03-11 18:32:45 +0530

    added beacon regions
    for men, women secitons

--- app/src/main/java/com/tavant/beaconretail/proximity/ProximityMarketing.java
+++ app/src/main/java/com/tavant/beaconretail/proximity/ProximityMarketing.java
@@ -1,6 +1,7 @@
 package com.tavant.beaconretail.proximity;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import com.estimote.sdk.Beacon;
@@ -23,10 +24,13 @@ public class ProximityMarketing extends Application{
 
 	private BeaconManager beaconManager;
 	private NotificationManager notificationManager;
-	private Region region;
+	private Region entryRegion,menSectionRegion,womenSectionRegion;
 	private boolean isProxyShoppingRunning = false;
 	
-	private static final int NOTIFICATION_ID = 123;
+	private static final int ENTRY_NOTIFICATION_ID          = 123;
+    private static final int MEN_NOTIFICATION_ID            = 456;
+    private static final int WOMEN_NOTIFICATION_ID          = 789;
+    private static final int EXIT_NOTIFICATION_ID           = 012;
 	private static final String TAG = "ProximityMarketing";
 	
 	public void startProximityMarketing(){
@@ -41,23 +45,40 @@ public class ProximityMarketing extends Application{
 	    beaconManager.setMonitoringListener(new MonitoringListener() {
 	      @Override
 	      public void onEnteredRegion(Region region, List<Beacon> beacons) {
-	        postNotification("Welcome to Proxy Store");
+              for(Beacon beacon : beacons){
+                  if (beacon.getMinor() == 59688){
+                      //entry beacon detected
+                      postNotification("Welcome to Tavant Retail Store",ENTRY_NOTIFICATION_ID);
+                  }else if(beacon.getMinor() == 39431){
+                      //men section beacon detected
+                      postNotification("We have exciting offers for you in Men section",MEN_NOTIFICATION_ID);
+                  }else if(beacon.getMinor() == 41861){
+                      //women section beacon detected
+                      postNotification("We have exciting offers for you in Women section", WOMEN_NOTIFICATION_ID);
+                  }
+              }
+
 	      }
 
 	      @Override
 	      public void onExitedRegion(Region region) {
-	        postNotification("Exited region");
+            if(region.getMinor() == 59688)
+	        postNotification("Thanks for visiting Tavant Retail Store",EXIT_NOTIFICATION_ID);
 	      }
 	    });
 		
-	    region = new Region("regionId", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 64990, 59688);
-	    
-	    notificationManager.cancel(NOTIFICATION_ID);
+	    entryRegion         = new Region("entryId", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 64990, 59688);
+	    menSectionRegion    = new Region("menSectionBeacon", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 32673, 39431);
+        womenSectionRegion  = new Region("WomenSectionBeacon", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 36164, 41861);
+
+	    //notificationManager.cancel(NOTIFICATION_ID);
 	    beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
 	      @Override
 	      public void onServiceReady() {
 	        try {
-	          beaconManager.startMonitoring(region);
+	          beaconManager.startMonitoring(entryRegion);
+              beaconManager.startMonitoring(menSectionRegion);
+              beaconManager.startMonitoring(womenSectionRegion);
 	        } catch (RemoteException e) {
 	          Log.d(TAG, "Error while starting monitoring");
 	        }
@@ -72,24 +93,25 @@ public class ProximityMarketing extends Application{
 		return beaconManager;
 	}
 	
-	private void postNotification(String msg) {
+	private void postNotification(String msg,int notificationId) {
 		    Intent notifyIntent = new Intent(ProximityMarketing.this, OffersActivity.class);
 		    notifyIntent.putExtra("Offer", msg);
 		    notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            Random random = new Random();
 		    PendingIntent pendingIntent = PendingIntent.getActivities(
 		    		ProximityMarketing.this,
-		        0,
+		        random.nextInt(),
 		        new Intent[]{notifyIntent},
 		        PendingIntent.FLAG_UPDATE_CURRENT);
 		    Notification notification = new Notification.Builder(ProximityMarketing.this)
 		        .setSmallIcon(R.drawable.beacon_gray)
-		        .setContentTitle("Notify Demo")
+		        .setContentTitle("Tavant Retail")
 		        .setContentText(msg)
 		        .setAutoCancel(true)
 		        .setContentIntent(pendingIntent)
 		        .build();
 		    notification.defaults |= Notification.DEFAULT_SOUND;
 		    notification.defaults |= Notification.DEFAULT_LIGHTS;
-		    notificationManager.notify(NOTIFICATION_ID, notification);
+		    notificationManager.notify(notificationId, notification);
 	}
 }
